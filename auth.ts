bff4427dbb343074eaf3d78aa3f60fc281ce9969
JVB <jay.bansal@tuskinvest.com>
2026-02-22 05:01:03 +0000

    Add trustHost, fix null safety for includes() calls

--- auth.ts
+++ auth.ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import MicrosoftEntraID from "next-auth/providers/microsoft-entra-id";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  trustHost: true,
   providers: [
     MicrosoftEntraID({
       clientId: process.env.AUTH_MICROSOFT_ENTRA_ID_ID!,
